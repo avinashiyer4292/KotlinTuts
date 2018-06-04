10792ba6d74d7a12e3b70eb5b9d756ae6ef08c97
aviyer <aviyer@stubhub.com>
2018-06-03 19:18:48 -0700

    Updated mainactivity animation, removed TODO errors

--- app/src/main/java/com/app/aviyer/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/app/aviyer/firstkotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package com.app.aviyer.firstkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,11 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val initialTextViewTranslationy = textViewProgress;
+        val initialTextViewTranslationy = textViewProgress.translationY;
 
         seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
                 textViewProgress.text = progress.toString();
 
                 val translationDistance = (initialTextViewTranslationy + progress *
@@ -24,12 +26,18 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+                Log.d("On start tracking","Start now!!!");
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+                Log.d("On stop tracking","Stop now!!!");
             }
+        })
+        reset_btn.setOnClickListener(View.OnClickListener {
+            seekBar.progress=0
+            textViewProgress.animate().setDuration(500).rotationBy(360f).translationY(initialTextViewTranslationy);
         })
     }
 }
