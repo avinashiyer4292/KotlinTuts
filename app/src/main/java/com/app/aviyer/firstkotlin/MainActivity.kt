5dfda5a66d9bc3bed360ce925f447f5851d27447
aviyer <aviyer@stubhub.com>
2018-06-03 19:02:09 -0700

    Added main activity , setting textview val

--- app/src/main/java/com/app/aviyer/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/app/aviyer/firstkotlin/MainActivity.kt
@@ -2,11 +2,34 @@ package com.app.aviyer.firstkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val initialTextViewTranslationy = textViewProgress;
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                textViewProgress.text = progress.toString();
+
+                val translationDistance = (initialTextViewTranslationy + progress *
+                        resources.getDimension(R.dimen.animstep) * -1);
+
+                textViewProgress.animate().translationY(translationDistance);
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
